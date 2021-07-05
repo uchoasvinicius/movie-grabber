faba05ece763ba8e08e5ba3b366bc3f2f9b430a3
Vinicius Uchoas <viniciusuchoas@outlook.com>
2021-07-04 22:15:59 -0300

    refactor: change RemoteDiscoverMovies to use faker

--- src/data/usecases/discover-movies/remote-discover-movies.spec.ts
+++ src/data/usecases/discover-movies/remote-discover-movies.spec.ts
@@ -1,12 +1,13 @@
 import { RemoteDiscoverMovies } from './remote-discover-movies'
 import { HttpGetClientSpy } from '../../test/mock-http-client'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteDiscoverMovies
   httpGetClientSpy: HttpGetClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpGetClientSpy = new HttpGetClientSpy()
   const sut = new RemoteDiscoverMovies(url, httpGetClientSpy)
   return {
@@ -17,7 +18,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe('RemoteDiscoverMovies', function () {
   test('Should call HttpGetClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpGetClientSpy } = makeSut(url)
     await sut.list()
     expect(httpGetClientSpy.url).toBe(url)
